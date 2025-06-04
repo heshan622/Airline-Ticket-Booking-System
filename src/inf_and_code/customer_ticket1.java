2a18027a27b10722159b81d61fba7b8f8fe2b189
Pasindu Heshan <p.heshan622@gmail.com>
2025-06-04 11:52:34 +0530

    Update customer_ticket1.java

--- src/inf_and_code/customer_ticket1.java
+++ src/inf_and_code/customer_ticket1.java
@@ -456,7 +456,7 @@ public class customer_ticket1 extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        this.dispose();
+        System.exit(0);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
